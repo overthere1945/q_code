5b517624fc9f8c2b21aad03b6c8aa7158dbb4b93
hyungchul.won <hyungchul.won@lge.com>
2026-05-14 17:02:41 +0900

    2026.05.14 add eAI

---
+++ non_HLOS/adsp_proc/eai/QNN/QnnSdkBuildId.h
@@ -0,0 +1,19 @@
+//==============================================================================
+//
+// Copyright (c) 2021-2026 Qualcomm Technologies, Inc.
+// All Rights Reserved.
+// Confidential and Proprietary - Qualcomm Technologies, Inc.
+//
+//==============================================================================
+
+//=============================================================================
+// !!! This is an auto-generated file. Do NOT modify manually !!!
+//=============================================================================
+
+#ifndef QNN_SDK_BUILD_ID_H
+#define QNN_SDK_BUILD_ID_H
+
+/// QNN SDK build id
+#define QNN_SDK_BUILD_ID "v2.44.0.260225143659"
+
+#endif  // QNN_SDK_BUILD_ID_H
